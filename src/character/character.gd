fe031d81337c39ffc51f7033a8d060261b7b8df9
Mike Dupree <mikerdupree@gmail.com>
2023-01-09 22:16:58 -0800

    adds hurt and hit boxes

--- src/character/character.gd
+++ src/character/character.gd
@@ -3,8 +3,16 @@ extends CharacterBody2D
 
 const SPEED = 50.0
 
-@onready var sprite := $Sprite2D
+@export var health := 100
+@export var armor := 5
 
+@onready var sprite := $Sprite2D
+@onready var hurtbox := $HurtBox
+
+func _ready() -> void:
+	hurtbox.damage.connect(take_damage)
+	
+	
 func _physics_process(_delta: float) -> void:
 	move()
 
@@ -15,9 +23,23 @@ func move() -> void:
 	direction.y = Input.get_axis("up", "down")
 	velocity = direction.normalized() * SPEED
 	
+	face_direction()
+	move_and_slide()
+
+
+func face_direction() -> void:
+	var direction := Vector2.ZERO
+	direction.x = Input.get_axis("left", "right")
 	if direction.x > 0: 
 		sprite.flip_h = false
-	else:
+	if direction.x < 0:
 		sprite.flip_h = true
 
-	move_and_slide()
+func take_damage(amount: int) -> void:
+	print('dmg amt', amount)
+	health -= calculate_damage(amount)
+	print('Health:', health)
+
+func calculate_damage(amount: int) -> int:
+	# calc all modifiers here
+	return amount
