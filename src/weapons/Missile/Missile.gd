ffc1842d1f5c601c687b37ab47ad10deb5373acf
Michael <mikerdupree@gmail.com>
2023-01-23 22:40:21 -0800

    missile homing and rotation (#3)

--- src/weapons/Missile/Missile.gd
+++ src/weapons/Missile/Missile.gd
@@ -1,25 +1,34 @@
 class_name Missile
 extends Area2D
 
-var speed := 250
+var max_speed := 250
+var current_speed := 0
+var torque := 5
 var direction: Vector2
 var velocity := Vector2.ZERO
 var target
-	
+@onready var sprite := $Sprite2D as Sprite2D
+@onready var ready_to_home = false
+const SMOOTH_SPEED = 5.0
 	
 func _ready() -> void:
 	pass
 
 
 func _physics_process(delta) -> void:
-	if not target:
-		velocity.x = direction.x * speed * delta
-		velocity.y = direction.y * speed * delta
-		translate(velocity)
-		return
-	
-	look_at(target.global_position)
-	position = position.move_toward(target.global_position, speed * delta)
+	_handle_initial_direction()
+	_handle_throttle()
+
+	if ready_to_home:
+		if not target:
+			_handle_no_target(delta)
+			return
+		# Ease the rotation towards the target
+		rotation = lerp_angle(rotation, -direction.x, delta * SMOOTH_SPEED)
+		# Move towards target
+		position = position.move_toward(target.global_position, current_speed * delta)
+	else:
+		_initial_burst(delta)
 
 
 func _on_visible_on_screen_notifier_2d_screen_exited() -> void:
@@ -29,3 +38,32 @@ func _on_visible_on_screen_notifier_2d_screen_exited() -> void:
 func _on_collision_counter_max_collisions_reached() -> void:
 	queue_free()
 
+
+func _handle_initial_direction() -> void:
+	if direction.x < 0:
+		sprite.set_rotation_degrees(270)
+	if direction.y < 0:
+		sprite.set_rotation_degrees(180)
+	if direction.y > 0:
+		sprite.set_rotation_degrees(0)
+
+
+func _handle_throttle() -> void:
+	if current_speed < max_speed:
+		current_speed += torque
+
+
+func _handle_no_target(delta) -> void:
+	velocity.x = direction.x * current_speed * delta
+	velocity.y = direction.y * current_speed * delta
+	translate(velocity)
+
+
+func _on_timer_timeout() -> void:
+	ready_to_home = true
+
+
+func _initial_burst(delta):
+	var tween = get_tree().create_tween()
+	tween.tween_property(self, "global_position:x", global_position.x + 16, 0.25).set_ease(Tween.EASE_OUT)
+	
--- src/weapons/Missile/MissileCannon.gd
+++ src/weapons/Missile/MissileCannon.gd
@@ -6,6 +6,8 @@ extends Node2D
 @onready var detector_radius = 2.5
 @onready var detector = $Detector
 var enemies_in_range = []
+
+
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	detector.scale = Vector2(detector_radius, detector_radius) 
@@ -23,8 +25,11 @@ func _on_timer_timeout() -> void:
 		return
 	missileNode.direction = samus.face_direction
 	missileNode.global_position = samus.global_position
+
 	if enemies_in_range.size() > 0:
+		#If enemy in range target the first one
 		missileNode.target = enemies_in_range[0]
+	# TODO create missile animation and control the missiles start angle
 	get_tree().get_root().add_child(missileNode)
 
 
